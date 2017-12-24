cfe9584da350d31454b2387c55a1835dd754978f
Sarp Saatçıoğlu <sarpsaatci@gmail.com>
2017-12-25 00:06:25 +0300

    bug fix recording trials

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ var https = require('https');
 var argv = minimist(process.argv.slice(2), {
   default: {
       as_uri: "https://localhost:8443/",
-      ws_uri: "ws://localhost:8888/kurento"
+      ws_uri: "ws://localhost:8888/kurento",
 	  file_uri: "file:///tmp/kurento-hello-world-recording.mp4"
   }
 });
