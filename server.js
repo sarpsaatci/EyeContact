30a3d1c57549355fa5d8dd43705ada6d9d77141f
Sarp Saatçıoğlu <sarpsaatci@gmail.com>
2018-02-03 20:52:45 +0300

    fct

--- server.js
+++ server.js
@@ -292,7 +292,7 @@ wss.on('connection', function(ws) {
 
 function getFrame(frame, ws)
 {
-  var frameUrl = url.createObjectURL(frame.blob);
+  var frameUrl = window.URL.createObjectURL(frame.blob);
   //sendUrl(url);
   return true;  
 }
