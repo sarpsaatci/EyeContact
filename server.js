38d04b3e62c880b2fc7ce822b772ea2f9b0e695d
Sarp Saatçıoğlu <sarpsaatci@gmail.com>
2018-02-03 19:07:24 +0300

    fct

--- server.js
+++ server.js
@@ -264,7 +264,7 @@ wss.on('connection', function(ws) {
             break;
 
         case 'frame':
-            ws.send(message.blob));
+            ws.send(message.blob);
             //getFrame(message);
             // ws.send(JSON.stringify({
             //    id : 'frameUrl',
