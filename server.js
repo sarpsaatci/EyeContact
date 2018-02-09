9bf42b6d360ad106b74036c4bef141028e8daa45
Sarp Saatçıoğlu <sarpsaatci@gmail.com>
2018-02-09 12:10:24 +0300

    cmd

--- server.js
+++ server.js
@@ -326,6 +326,7 @@ function getFrame(frame)
   // Returns a Promise
   imageDataURI.outputFile(dataURI, filePath).then(res => 
     shell.exec('./../OpenFace/build/bin/FeatureExtraction -fdir ./frames/callee -of ../OpenFace/output/' + frame.path + '.txt -q'),
+    filePath = res,
     console.log(res)
   );
   
