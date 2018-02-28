0d057683170593d88a41f1e2a129abcd4ef16175
Sarp Saatçıoğlu <sarpsaatci@gmail.com>
2018-02-28 17:59:17 +0300

    out

--- server.js
+++ server.js
@@ -78,8 +78,8 @@ var watcher = fswatch.watch('/root/OpenFace/outputs/deneme.csv', {
 var log = console.log.bind(console);
 
 watcher
-  //.on('add', path => log(`File ${path} has been added`))
-  .on('change', path => log(`File ${path} has been changed`))
+  .on('add', path => log(`File ${path} has been added`))
+  // .on('change', path => log(`File ${path} has been changed`))
   .on('unlink', path => log(`File ${path} has been removed`));
 
 
