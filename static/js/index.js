6a8aaed0fa0483f57631f7b875ed3a586e59185f
sarpsaatci <sarpsaatci@gmail.com>
2018-02-28 23:17:26 +0300

    po

--- static/js/index.js
+++ static/js/index.js
@@ -203,8 +203,8 @@ ws.onmessage = function(message) {
     console.log(message);
     break;
   case 'output':
-    console.log("aha aha aha");
-    //printOutput(message);
+    // console.log("aha aha aha");
+    printOutput(message);
     break;
 	default:
 		console.error('Unrecognized message', parsedMessage);
