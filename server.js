f20c677705dfde233c4e4e3b255dee9c98c5ac8c
Sarp Saatçıoğlu <sarpsaatci@gmail.com>
2018-03-18 23:14:54 +0300

    fa

--- server.js
+++ server.js
@@ -34,8 +34,7 @@ var shell = require('shelljs');
 var nbind = require('nbind');
 var lib = nbind.init().lib;
 var fswatch = require('chokidar');
-// var firebase = require('firebase');
-// var firebaseui = require('firebaseui');
+var firebase = require('firebase');
 
 
 var argv = minimist(process.argv.slice(2), {
