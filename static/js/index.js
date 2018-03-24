699e09d84c3fd4322f33c3d93d0159f4b92ffb42
sarpsaatci <sarpsaatci@gmail.com>
2018-03-25 02:59:29 +0300

    fa

--- static/js/index.js
+++ static/js/index.js
@@ -220,7 +220,7 @@ ws.onmessage = function(message) {
 
 function manageUser(currentUser)
 {
-  if(currentUser.providerData.providerId == "google.com")
+  if(currentUser.providerData.providerId == 'google.com')
     sendMessage({
       id : 'user',
       currentUser : currentUser
