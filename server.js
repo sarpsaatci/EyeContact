a04a49c086c6dcd0cb5c7a765799b0e70f9f057b
Sarp Saatçıoğlu <sarpsaatci@gmail.com>
2018-04-26 10:01:49 +0300

    set

--- server.js
+++ server.js
@@ -641,6 +641,7 @@ function register(id, userName, contacts, email, settings, ws, callback) {
               console.log(result);
             });
             return;
+          }
         }
         else {
           console.log(userName + ' added to db');
