bbc8127d6e1e18781864b3716bf4e99d69f012d3
Sarp Saatçıoğlu <sarpsaatci@gmail.com>
2018-04-26 10:40:07 +0300

    set

--- server.js
+++ server.js
@@ -634,20 +634,19 @@ function register(id, userName, contacts, email, settings, ws, callback) {
       newUser.save(function (err, newUser) {
         if (err) {
           if(err.code == 11000) {
-            if(console.log('User ' + userName + ' already exists.')) {
-              changeSettings = true;
-              console.log(changeSettings);
-              if(changeSettings) {
-                newUser.findOneAndUpdate({email: newUser.email}, newUser, {new: true, upsert: true, setDefaultsOnInsert: true}, function(error, result) {
-                  if(error){
-                    console.log("Something wrong when updating data!");
-                  }
+            console.log('User ' + userName + ' already exists.');
+            changeSettings = true;
+            console.log(changeSettings);
+            if(changeSettings) {
+              newUser.findOneAndUpdate({email: newUser.email}, newUser, {new: true, upsert: true, setDefaultsOnInsert: true}, function(error, result) {
+                if(error){
+                  console.log("Something wrong when updating data!");
+                }
 
-                  console.log(result);
+                console.log(result);
 
-                  changeSettings = false;
-                });
-              }
+                changeSettings = false;
+              });
             }
           }
         }
