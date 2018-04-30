f557740d5750082d2d905187ba7eea4bffd1cd1b
Sarp Saatçıoğlu <sarpsaatci@gmail.com>
2018-04-30 11:34:29 +0300

    ui

--- static/js/index.js
+++ static/js/index.js
@@ -303,6 +303,32 @@ function speakAutocompleteItems(items)
     console.log(item);
     window.speechSynthesis.speak(new SpeechSynthesisUtterance(item));
   });
+
+  window.speechSynthesis.speak(new SpeechSynthesisUtterance('please say your choice'));
+
+  if (window.hasOwnProperty('webkitSpeechRecognition')) {
+
+    var recognition = new webkitSpeechRecognition();
+
+    recognition.continuous = false;
+    recognition.interimResults = false;
+
+    recognition.lang = "en-US";
+    recognition.start();
+
+    recognition.onresult = function(e) {
+      console.log(e.results[0][0].transcript);
+      let number = parseInt(e.results[0][0].transcript);
+      recognition.stop();
+      let peer = autocompleteItems[number-1];
+      makeCall(peer.substring(peer.indexOf('(')+1, peer.indexOf(')')));
+    };
+
+    recognition.onerror = function(e) {
+      recognition.stop();
+    }
+  }
+
 }
 
 function dummyFace(line)
