bf5391916bfecb871a783b02d5774e9e853456f3
Sarp Saatçıoğlu <sarpsaatci@gmail.com>
2018-05-08 18:20:35 +0300

    bar

--- server.js
+++ server.js
@@ -709,7 +709,6 @@ function incomingCallResponse(calleeId, from, callResponse, calleeSdp, ws) {
     //   .on('addDir', path => watcher.add(path, caller, callee));
 
     follower = follow('/root/OpenFace/outputFile.txt', options = {persistent: true, catchup: true});
-    allFollower = follow('/root/OpenFace/emotionsOutputFile.txt', options = {persistent: true, catchup: true});
 
     follower.on('line', function(filename, line) {
       // console.log('OpenFace: '+line);
@@ -719,74 +718,77 @@ function incomingCallResponse(calleeId, from, callResponse, calleeSdp, ws) {
         caller.sendMessage({
           id: 'capture'
         });
+
+        allFollower = follow('/root/OpenFace/emotionsOutputFile.txt', options = {persistent: true, catchup: true});
+
+        allFollower.on('line', function(filename, line) {
+          // console.log('OpenFace: '+line);
+          if(line.includes('$'))
+          {
+            if(line.includes('anger')) {
+              caller.sendMessage({
+                id: 'tracking',
+                emotion: 'anger',
+                data: line.substr(line.indexOf(':'))
+              });
+            }
+            if(line.includes('disgust')) {
+              caller.sendMessage({
+                id: 'tracking',
+                emotion: 'disgust',
+                data: line.substr(line.indexOf(':'))
+              });
+            }
+            if(line.includes('fear')) {
+              caller.sendMessage({
+                id: 'tracking',
+                emotion: 'fear',
+                data: line.substr(line.indexOf(':'))
+              });
+            }
+            if(line.includes('happiness')) {
+              caller.sendMessage({
+                id: 'tracking',
+                emotion: 'happiness',
+                data: line.substr(line.indexOf(':'))
+              });
+            }
+            if(line.includes('sadness')) {
+              caller.sendMessage({
+                id: 'tracking',
+                emotion: 'sadness',
+                data: line.substr(line.indexOf(':'))
+              });
+            }
+            if(line.includes('surprise')) {
+              caller.sendMessage({
+                id: 'tracking',
+                emotion: 'surprised',
+                data: line.substr(line.indexOf(':'))
+              });
+            }
+            if(line.includes('neutral')) {
+              caller.sendMessage({
+                id: 'tracking',
+                emotion: 'neutral',
+                data: line.substr(line.indexOf(':'))
+              });
+            }
+          }
+          else {
+            caller.sendMessage({
+              id: 'tracking',
+              emotion: 'n/a',
+              data: 0
+            });
+          }
+        });
+
       }
       else {
         caller.sendMessage({
           id: 'openFace',
           data: line
-        });
-      }
-    });
-
-    allFollower.on('line', function(filename, line) {
-      // console.log('OpenFace: '+line);
-      if(line.includes('$'))
-      {
-        if(line.includes('anger')) {
-          caller.sendMessage({
-            id: 'tracking',
-            emotion: 'anger',
-            data: line.substr(line.indexOf(':'))
-          });
-        }
-        if(line.includes('disgust')) {
-          caller.sendMessage({
-            id: 'tracking',
-            emotion: 'disgust',
-            data: line.substr(line.indexOf(':'))
-          });
-        }
-        if(line.includes('fear')) {
-          caller.sendMessage({
-            id: 'tracking',
-            emotion: 'fear',
-            data: line.substr(line.indexOf(':'))
-          });
-        }
-        if(line.includes('happiness')) {
-          caller.sendMessage({
-            id: 'tracking',
-            emotion: 'happiness',
-            data: line.substr(line.indexOf(':'))
-          });
-        }
-        if(line.includes('sadness')) {
-          caller.sendMessage({
-            id: 'tracking',
-            emotion: 'sadness',
-            data: line.substr(line.indexOf(':'))
-          });
-        }
-        if(line.includes('surprise')) {
-          caller.sendMessage({
-            id: 'tracking',
-            emotion: 'surprised',
-            data: line.substr(line.indexOf(':'))
-          });
-        }
-        if(line.includes('neutral')) {
-          caller.sendMessage({
-            id: 'tracking',
-            emotion: 'neutral',
-            data: line.substr(line.indexOf(':'))
-          });
-        }
-      }
-      else {
-        caller.sendMessage({
-          id: 'tracking',
-          emotion: 'n/a',
-          data: 0
         });
       }
     });
